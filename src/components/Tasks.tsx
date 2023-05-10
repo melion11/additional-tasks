b3d267b6b107a27e3ac6582e7c29fa4ed94f2f02
melion11 <gphooenix@gmail.com>
2023-05-10 22:13:09 +0300

    completed additional task with animate and useRef

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,44 +1,45 @@
 import React from "react";
-import {DataType} from "../App";
-
-type TasksPropsType = {
-    data: DataType
-}
-
-const Tasks = (props: TasksPropsType) => {
-
-    const mapTasks = props.data.tasks.map(elem => {
-        return (
-            <li>
-                <span>{elem.taskId}</span>
-                <span>{elem.title}</span>
-                <span>{elem.isDone}</span>
-            </li>
-
-    );
-    })
-
-    const mapStudents = props.data.students.map(elem => {
-        return (
-            <li>
-                <span>{elem}</span>
-            </li>
-        );
-    })
-
-    return (
-        <div>
-            <h1>{props.data.title}</h1>
-            <ul>
-                {mapTasks}
-            </ul>
-            <ul>
-                {mapStudents}
-            </ul>
-
-
-        </div>
-    );
-}
-
-export default Tasks;
+// import {DataType} from "../App";
+//
+// type TasksPropsType = {
+//     data: DataType
+// }
+//
+//
+// const Tasks = (props: TasksPropsType) => {
+//
+//     const mapTasks = props.data.tasks.map(elem => {
+//         return (
+//             <li>
+//                 <span>{elem.taskId}</span>
+//                 <span>{elem.title}</span>
+//                 <span>{elem.isDone}</span>
+//             </li>
+//
+//     );
+//     })
+//
+//     const mapStudents = props.data.students.map(elem => {
+//         return (
+//             <li>
+//                 <span>{elem}</span>
+//             </li>
+//         );
+//     })
+//
+//     return (
+//         <div>
+//             <h1>{props.data.title}</h1>
+//             <ul>
+//                 {mapTasks}
+//             </ul>
+//             <ul>
+//                 {mapStudents}
+//             </ul>
+//
+//
+//         </div>
+//     );
+// }
+//
+// export default Tasks;
